12999d9b0d01d951323b4aa1dc8a8a70bb2c8904
Javier-15-03 <javiercellan8@gmail.com>
2025-09-24 19:06:56 -0500

    Cara de Iniciar sesion

--- OSC-Frontend/src/app/acceso/login/login.ts
+++ OSC-Frontend/src/app/acceso/login/login.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+    @Output() cerrarModal = new EventEmitter<void>();
+
+  usuario = '';
+  password = '';
+
+  login() {
+    console.log('Login con:', this.usuario);
+    // Si login exitoso:
+    this.cerrarModal.emit();
+  }
+
 
 }
--- OSC-Frontend/src/app/app.ts
+++ OSC-Frontend/src/app/app.ts
@@ -1,12 +1,35 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Login } from "./acceso/login/login";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [CommonModule, FormsModule, Login],
+  template:`
+    <header>
+      <button (click)="abrirLogin()">👤</button>
+    </header>
+    
+    <div *ngIf="mostrarLogin" class="modal-overlay">
+      <app-login (cerrarModal)="cerrarLogin()"></app-login>
+    </div>
+  `,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('OSC-Frontend');
+  title = 'OSC-Frontend';
+  
+  // Agregar estas líneas:
+  mostrarLogin = false;
+
+  abrirLogin() {
+    this.mostrarLogin = true;
+  }
+
+  cerrarLogin() {
+    this.mostrarLogin = false;
+
+}
 }
