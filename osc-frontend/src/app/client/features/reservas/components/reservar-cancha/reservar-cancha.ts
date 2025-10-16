4fe30f5e1f47816b2262d8a380714f25d59d698b
Noemi2005 <sandycajamarca828@gmail.com>
2025-10-16 16:46:14 -0500

    s

--- osc-frontend/src/app/client/features/reservas/components/reservar-cancha/reservar-cancha.ts
+++ osc-frontend/src/app/client/features/reservas/components/reservar-cancha/reservar-cancha.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-reservar-cancha',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './reservar-cancha.html',
   styleUrl: './reservar-cancha.css'
 })
