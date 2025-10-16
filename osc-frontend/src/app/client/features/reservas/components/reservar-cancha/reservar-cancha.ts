8484fa7ae6d4c36552e9167a21037b5e41113e1e
Noemi2005 <sandycajamarca828@gmail.com>
2025-10-16 15:49:06 -0500

    Componente Reserva Cancha

---
+++ osc-frontend/src/app/client/features/reservas/components/reservar-cancha/reservar-cancha.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reservar-cancha',
+  imports: [],
+  templateUrl: './reservar-cancha.html',
+  styleUrl: './reservar-cancha.css'
+})
+export class ReservarCancha {
+
+}
