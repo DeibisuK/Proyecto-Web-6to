2b39b2eaba7c56ff87cd6636d6c5349add6720d8
kinjirobruh96 <garystevenbarreirozambrano@gmail.com>
2026-01-01 16:45:08 -0500

    Referencia a Carrusel de Articulos eliminada en Home

--- osc-frontend/src/app/features/client/pages/home/pages/home-page/home-page.ts
+++ osc-frontend/src/app/features/client/pages/home/pages/home-page/home-page.ts
@@ -3,12 +3,11 @@ import { Contact } from '../../../contact/contact';
 import { HoverGalleryComponent } from '../../components/hover-gallery/hover-gallery';
 import { PreguntasFrecuentes } from "../../components/preguntas-frecuentes/preguntas-frecuentes";
 import { CaracteristicasPrincipales } from "../../components/caracteristicas-principales/caracteristicas-principales";
-import { ArticlesCarouselComponent } from '../../components/articles-carousel/articles-carousel';
 import { ComoFuncionaComponent } from '../../components/como-funciona/como-funciona';
 
 @Component({
   selector: 'app-home',
-  imports: [ArticlesCarouselComponent, Contact, HoverGalleryComponent, PreguntasFrecuentes, CaracteristicasPrincipales, ComoFuncionaComponent],
+  imports: [Contact, HoverGalleryComponent, PreguntasFrecuentes, CaracteristicasPrincipales, ComoFuncionaComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
